b53809f53bb918846b81d9625cacee15120509df
khrchvnkv <harchevnikovdenis@mail.ru>
2023-06-08 23:45:10 +0300

    initial

---
+++ MyConference/Pages/SchedulePage.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConference.Pages
+{
+    public partial class SchedulePage : ContentPage
+    {
+        public SchedulePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
